1f450fdd8c72e63d9c9ea4f30b9dfc740b439649
jaymar921 <jayharronabejar@gmail.com>
2024-04-03 19:09:33 +0800

    Updated portfolio link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = ({className}) => {
   return (
     <footer className={`w-[100%] ${className} font-minecraft text-blue-300`}>
-        2024 &copy; Made by <a className="text-blue-400" href='https://github.com/jaymar921' target='_blank'>Jaymar921</a>
+        2024 &copy; Made by <a className="text-blue-400" href='https://jayharronabejar.vercel.app/' target='_blank'>Jaymar921</a>
     </footer>
   )
 }
